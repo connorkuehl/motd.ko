c0238a1a44557b20c3e07700f13a1062fa0f93de
Connor Kuehl <cipkuehl@gmail.com>
2020-08-28 13:57:54 -0500

    Allocate a device number

--- motd.c
+++ motd.c
@@ -1,12 +1,44 @@
+#include "motd.h"
+
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/stat.h>
+
+static int motd_major = 0;
+module_param(motd_major, int, S_IRUGO);
+
+static int motd_minor = 0;
+module_param(motd_minor, int, S_IRUGO);
 
 static int __init motd_init(void)
 {
+	int result;
+	dev_t dev;
+
+	if (motd_major) {
+		dev = MKDEV(motd_major, motd_minor);
+		result = register_chrdev_region(dev, MOTD_NDEVS, MOTD_NAME);
+	} else {
+		result = alloc_chrdev_region(&dev, motd_minor, MOTD_NDEVS, MOTD_NAME);
+		motd_major = MAJOR(dev);
+	}
+
+	if (result < 0) {
+		printk(KERN_WARNING MOTD_NAME ": can't get major %d\n", motd_major);
+	}
+
 	return 0;
 }
 
 static void __exit motd_exit(void)
 {
+	dev_t dev = MKDEV(motd_major, motd_minor);
+
+	unregister_chrdev_region(dev, MOTD_NDEVS);
 }
 
 MODULE_AUTHOR("Connor Kuehl <cipkuehl@gmail.com>");
---
+++ motd.h
@@ -0,0 +1,9 @@
+#ifndef MODULE_MOTD_H_
+#define MODULE_MOTD_H_
+
+#define MOTD_MAJOR (0)
+#define MOTD_MINOR (0)
+#define MOTD_NDEVS (1)
+#define MOTD_NAME "motd"
+
+#endif // MODULE_MOTD_H_
